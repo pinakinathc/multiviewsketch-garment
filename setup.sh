96ff21d937002127cbb814c1264d4e132166f7b4
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2022-01-17 09:06:59 +0000

    removed redundant lines in miniconda

--- setup.sh
+++ setup.sh
@@ -4,10 +4,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p
 
 source ~/.bashrc
-source ~/miniconda/etc/profile.d/conda.sh
 conda create --name garment python=3.7 numpy scikit-image -c pytorch -y
 
-source ~/miniconda/etc/profile.d/conda.sh
 conda activate garment
 
 python --version
